58a0f1bbca4f3109ede0466ac497583975e2af5d
Leo Delprete <=>
2024-04-21 08:58:27 -0400

    First Commit

---
+++ Color Picker/Color Picker/Color_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_PickerApp.swift
+//  Color Picker
+//
+//  Created by Leo DelPrete on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Color_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
